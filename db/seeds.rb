a680086f895d2c142ba6867b18c42702666b17ff
isabellagh <isabellagh88@gmail.com>
2021-05-04 22:44:32 -0400

    associations working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+    
+    Task.create(name: Faker::Lorem.word, notes: Faker::Lorem.words, completed: Faker::Boolean.boolean, todo_list_id: rand(1..10))
+
+end
+
+10.times do
+    
+    TodoList.create(name: Faker::Lorem.word)
+
+end
